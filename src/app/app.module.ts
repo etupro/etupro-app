4d6201ad2edfeebfb609083025ef57fdf8cf283b
Maxime Lozach <maxime.lozach@protonmail.com>
2024-02-22 09:13:38 +0100

    base app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AppProperties} from "./app.properties";
+import {AngularFireModule} from "@angular/fire/compat";
+import {FirebaseOptions} from "firebase/app";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: AppProperties.FIREBASE_CONFIG.API_KEY,
+  authDomain: AppProperties.FIREBASE_CONFIG.AUTH_DOMAIN,
+  projectId: AppProperties.FIREBASE_CONFIG.PROJECT_ID,
+  storageBucket: AppProperties.FIREBASE_CONFIG.STORAGE_BUCKET,
+  messagingSenderId: AppProperties.FIREBASE_CONFIG.MESSAGING_SENDER_ID,
+  appId: AppProperties.FIREBASE_CONFIG.APP_ID,
+  measurementId: AppProperties.FIREBASE_CONFIG.MEASUREMENT_ID
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +24,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
