47fdabd97fa199ef9ec1881836a51a8fadadc540
Maxime Lozach <maxime.lozach@protonmail.com>
2024-03-05 15:55:45 +0100

    remove app.properties

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
-import {NgModule, OnInit} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {AppProperties} from "./app.properties";
 import {FirebaseOptions} from "firebase/app";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {getAuth, provideAuth} from "@angular/fire/auth";
