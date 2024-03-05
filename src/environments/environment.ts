1d0f039d8c43fa7bafba773ce85c7993bc4c3a84
Maxime Lozach <maxime.lozach@protonmail.com>
2024-03-05 15:46:17 +0100

    fix ci-cd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   isProduction: true,
   FIREBASE: {
-    API_KEY: "",
-    AUTH_DOMAIN: "",
-    PROJECT_ID: "",
-    STORAGE_BUCKET: "",
-    MESSAGING_SENDER_ID: "",
-    APP_ID: "",
-    MEASUREMENT_ID: "",
+    API_KEY: '',
+    AUTH_DOMAIN: '',
+    PROJECT_ID: '',
+    STORAGE_BUCKET: '',
+    MESSAGING_SENDER_ID: '',
+    APP_ID: '',
+    MEASUREMENT_ID: '',
   }
 };
