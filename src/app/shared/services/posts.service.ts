8da4392845baba6a55f3e5b2bf1ba5ba3c0726be
Maxime Lozach <maxime.lozach@protonmail.com>
2024-09-12 15:12:43 +0200

    feat: add delete post feature for own posts
    refs: https://github.com/etupro/etupro-app/issues/21

--- src/app/shared/services/posts.service.ts
+++ src/app/shared/services/posts.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Post } from "../models/post.model";
-import { SupabaseService } from "./supabase.service";
-import { DateTime } from "luxon";
-import { PostgrestSingleResponse } from "@supabase/supabase-js";
+import { Post } from '../models/post.model';
+import { SupabaseService } from './supabase.service';
+import { DateTime } from 'luxon';
 
 @Injectable({
   providedIn: 'root'
@@ -12,37 +11,76 @@ export class PostsService {
   constructor(private supabaseService: SupabaseService) {
   }
 
-  async getAllByTags(tags: string[] = []): Promise<PostgrestSingleResponse<Post[]>> {
-    return this.supabaseService.client
+  async getAllByTags(tags: string[] = []): Promise<Post[]> {
+    const response = await this.supabaseService.client
       .from('posts')
       .select('*, user_profiles(*)')
       .contains('tags', tags)
       .order('updated_at', {ascending: false})
       .limit(100);
+
+    if (response.error) {
+      throw response.error;
+    }
+
+    return response.data;
   }
 
-  async getById(id: number): Promise<PostgrestSingleResponse<Post | null>> {
-    return this.supabaseService.client
+  async getById(id: number): Promise<Post | null> {
+    const response = await this.supabaseService.client
       .from('posts')
       .select('*, user_profiles(*)')
       .eq('id', id)
       .maybeSingle();
+
+    if (response.error) {
+      throw response.error;
+    }
+
+    return response.data;
   }
 
-  async create(post: Post.Insert): Promise<PostgrestSingleResponse<null>> {
-    return this.supabaseService.client
+  async create(post: Post.Insert): Promise<Post> {
+    const response = await this.supabaseService.client
       .from('posts')
-      .insert(post);
+      .insert(post)
+      .select('*')
+      .single();
+
+    if (response.error) {
+      throw response.error;
+    }
+
+    return response.data;
   }
 
-  async update(id: number, post: Post.Update): Promise<PostgrestSingleResponse<null>> {
-    return this.supabaseService.client
+  async update(id: number, post: Post.Update): Promise<Post> {
+    const response = await this.supabaseService.client
       .from('posts')
       .update({
         ...post,
         id,
         updated_at: DateTime.now().toISO()
       })
+      .eq('id', id)
+      .select('*')
+      .single();
+
+    if (response.error) {
+      throw response.error;
+    }
+
+    return response.data;
+  }
+
+  async delete(id: number): Promise<void> {
+    const response = await this.supabaseService.client
+      .from('posts')
+      .delete()
       .eq('id', id);
+
+    if (response.error) {
+      throw response.error;
+    }
   }
 }
