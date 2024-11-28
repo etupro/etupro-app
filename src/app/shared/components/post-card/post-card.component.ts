822fa7a575401f1791650d54a4f3f06c11b11146
Maxime Lozach <maxime.lozach@protonmail.com>
2024-11-28 13:37:24 +0100

    feat: add miniatures on post page

--- src/app/shared/components/post-card/post-card.component.ts
+++ src/app/shared/components/post-card/post-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Post } from '../../models/post.model';
 import { CommonModule } from '@angular/common';
 import { MatCard } from '@angular/material/card';
@@ -7,8 +7,7 @@ import { SafeResourceUrl } from '@angular/platform-browser';
 import { Department } from '../../models/department.model';
 import { QueryPostTags } from '../../models/query-post-tags.model';
 import { EmitorStatusPipe } from '../../pipes/emitor-status/emitor-status.pipe';
-import { Map } from 'immutable';
-import { StorageService } from '../../services/storage.service';
+import { OrganizationMiniaturesComponent } from '../organization-miniatures/organization-miniatures.component';
 
 @Component({
   selector: 'app-post-card',
@@ -21,9 +20,10 @@ import { StorageService } from '../../services/storage.service';
     MatChipListbox,
     MatChipOption,
     EmitorStatusPipe,
+    OrganizationMiniaturesComponent,
   ]
 })
-export class PostCardComponent implements OnChanges {
+export class PostCardComponent {
 
   @Input() post: Post;
   @Input() coverUrl?: SafeResourceUrl | string;
@@ -31,25 +31,6 @@ export class PostCardComponent implements OnChanges {
   @Output() postClick = new EventEmitter<number>();
   @Output() tagClick = new EventEmitter<QueryPostTags>();
   @Output() organizationClick = new EventEmitter<number>();
-
-  organizationLogos: Map<number, (SafeResourceUrl | string)> = Map<number, (SafeResourceUrl | string)>();
-
-  constructor(private storageService: StorageService) {
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['post']) {
-      this.organizationLogos = Map<number, (SafeResourceUrl | string)>();
-      if (this.post.organizations) {
-        const organizationWithLogos = this.post.organizations
-          .filter(o => o.picture)
-          .slice(0, 3);
-        this.storageService.getSignedUrls(StorageService.BucketName.ORGANIZATION_IMAGES, organizationWithLogos.map(o => o.picture) as string[]).then(urls => {
-          this.organizationLogos = Map<number, (SafeResourceUrl | string)>(organizationWithLogos.map(o => [o.id, urls.get(o.picture as string) as string]));
-        });
-      }
-    }
-  }
 
   handlePostClick() {
     this.postClick.emit(this.post.id);
@@ -74,16 +55,5 @@ export class PostCardComponent implements OnChanges {
     this.tagClick.emit(new QueryPostTags({
       tags: [tag],
     }));
-  }
-
-  handleOrganizationClick(event: MouseEvent, organizationId: number) {
-    event.stopPropagation();
-    this.organizationClick.emit(organizationId);
-  }
-
-  handleKeyDown(event: KeyboardEvent, organizationId: number): void {
-    if (event.key === 'Enter' || event.key === ' ') {
-      this.organizationClick.emit(organizationId);
-    }
   }
 }
